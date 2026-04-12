d3282adb9469230acc3d50f83cbfb7d8eef08de1
Elikem Kuivi <eykuivi@gmail.com>
2026-04-12 16:11:25 -0700

    Initial Commit

---
+++ learnopengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learnopengl
+//
+//  Created by Elikem Kuivi on 4/12/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
